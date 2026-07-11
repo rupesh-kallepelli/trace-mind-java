d9d655201f7d15bb3984b88acb5415f68ee96d8e
rupesh_kallepelli <kallepallirupesh@gmail.com>
2026-07-11 21:14:20 +0530

    pom fix for orchestrator

--- trace-mind-agent/src/main/java/com/hcltech/trace/mind/agent/controller/ObservaAIController.java
+++ trace-mind-agent/src/main/java/com/hcltech/trace/mind/agent/controller/ObservaAIController.java
@@ -4,7 +4,6 @@ import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
